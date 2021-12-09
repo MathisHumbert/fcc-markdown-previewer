2ffe1c408e4c3b390f7674bfeb327bad5bed5c56
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-09 13:04:20 +0100

    progressé

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactMarkdown from 'react-markdown';
 import './index.css';
 
 const App = () => {
-  return <h1>hello world</h1>;
+  const [markdown, setMarkdown] = React.useState('');
+
+  return (
+    <main>
+      <textarea
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      ></textarea>
+      <article>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </article>
+    </main>
+  );
 };
 
 ReactDOM.render(
