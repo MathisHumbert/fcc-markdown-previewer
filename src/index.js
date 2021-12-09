ab5143acec433e99cf30c3272a5e51a095ea20ca
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-09 15:04:23 +0100

    project done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactMarkdown from 'react-markdown';
+import { marked } from 'marked';
 import './index.css';
 
+marked.setOptions({
+  breaks: true,
+});
+
 const App = () => {
-  const [markdown, setMarkdown] = React.useState('');
+  const renderer = new marked.Renderer();
+  const [markdown, setMarkdown] = React.useState(`
+  # H1
+  ## H2 
+  [title](https://www.example.com)
+  
+  code: \`<div></div>\`
+
+code clock: 
+\`\`\`
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+1. First item
+2. Second item
+3. Third item
+
+> Free Code Camp
+
+**Free Code Camp**
+
+![alt text](https://cdn.rawgit.com/Deftwun/e3756a8b518cbb354425/raw/6584db8babd6cbc4ecb35ed36f0d184a506b979e/free-code-camp-logo.svg
+)
+  `);
 
   return (
-    <main>
+    <main className="markdown">
       <textarea
+        className="input"
         value={markdown}
         onChange={(e) => setMarkdown(e.target.value)}
+        id="editor"
       ></textarea>
-      <article>
-        <ReactMarkdown>{markdown}</ReactMarkdown>
-      </article>
+
+      <article
+        className="result"
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked(markdown, { renderer }),
+        }}
+      ></article>
     </main>
   );
 };
